a446d1f54abfdf8cae108e3bc301f7f3e1781766
Hafid Mukhlasin <milisstudio@gmail.com>
2016-11-25 02:13:18 +0700

    revisi 3

--- sources/8/A.2.4.php
+++ sources/8/A.2.4.php
@@ -18,7 +18,14 @@ AppAsset::register($this);
 
 <div class="wrap">
     <div class="container">
-        <?= $content ?>
+        <div class="row">
+			<div class="col-md-2">        
+				<?= $this->blocks['sidebar']; ?>   
+			</div>
+			<div class="col-md-10">
+				<?= $content; ?>
+			</div>
+		</div>
     </div>
 </div>
 
